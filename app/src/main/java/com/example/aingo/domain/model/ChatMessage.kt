3bdd8d7cdd5f58f4926cf95cf12dfa5287b33fa6
NureDudukovOleksandr <oleksandr.dudukov@nure.ua>
2026-06-19 14:27:18 +0300

    Delete app directory

--- app/src/main/java/com/example/aingo/domain/model/ChatMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aingo.domain.model
-
-
-data class ChatMessage(
-    val text: String = "",
-    @field:JvmField
-    val isUser: Boolean = false,
-    val timestamp: Long = 0
-)
--- app/src/main/java/com/example/aingo/domain/repository/ChatRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aingo.domain.repository
-
-import com.example.aingo.domain.model.ChatMessage
-import kotlinx.coroutines.flow.Flow
-
-interface ChatRepository {
-    suspend fun saveMessage(message: ChatMessage)
-    fun getMessages(): Flow<List<ChatMessage>>
-}
